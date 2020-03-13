331706949a6641fde897b59fb4767875138c900b
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-13 12:15:11 -0600

    fixed the S. again.

--- middle/middle.js
+++ middle/middle.js
@@ -1,96 +1,96 @@
-const projects = require('../data/helpers/projectModel')
-const actions = require('../data/helpers/actionModel')
+const project = require("../data/helpers/projectModel");
+const actions = require("../data/helpers/actionModel");
 module.exports = {
-    logger,
-    validateProjectId,
-    validateActionId,
-    validateAction,
-    validateProject
+  logger,
+  validateProjectId,
+  validateActionId,
+  validateAction,
+  validateProject
 };
 
 function logger(req, res, next) {
-    res.send(`${Date.now()}, ${req.method}, ${req.originalUrl}`);
-    next();
+  res.send(`${Date.now()}, ${req.method}, ${req.originalUrl}`);
+  next();
 }
 
 function validateProjectId(req, res, next) {
-    projects.getById(req.params.id).then(thing => {
-        if (thing) {
-            req.project = thing;
-            next();
-        } else {
-            return res.status(400).json({
-                message: "invalid id"
-            });
-        }
-    });
+  projects.getById(req.params.id).then(thing => {
+    if (thing) {
+      req.project = thing;
+      next();
+    } else {
+      return res.status(400).json({
+        message: "invalid id"
+      });
+    }
+  });
 }
 
 function validateActionId(req, res, next) {
-    actions.getById(req.params.id).then(thing => {
-        if (thing) {
-            req.action = thing;
-            next();
-        } else {
-            return res.status(400).json({
-                message: "invalid user id"
-            });
-        }
-    });
+  actions.getById(req.params.id).then(thing => {
+    if (thing) {
+      req.action = thing;
+      next();
+    } else {
+      return res.status(400).json({
+        message: "invalid user id"
+      });
+    }
+  });
 }
 
 function validateAction(req, res, next) {
-    if (!req.body) {
-        return res.status(400).json({
-            message: "missing all data"
-        });
-    }
+  if (!req.body) {
+    return res.status(400).json({
+      message: "missing all data"
+    });
+  }
 
-    if (!req.body.project_id) {
-        return res.status(400).json({
-            message: "missing required project id"
-        });
-    }
-    if (!project.getById(req.body.project_id)) {
-        return res.status(400).json({
-            message: "please enter a valid project_id"
-        });
-    }
-    if (!req.body.description) {
-        return res.status(400).json({
-            message: "please enter a description"
-        });
-    }
-    if (req.body.description.length > 128) {
-        return res.status(400).json({
-            message: "only 128 characters please"
-        });
-    }
-    if (!req.body.notes) {
-        return res.status(400).json({
-            message: "please enter a couple of notes"
-        });
-    }
-    if (req.body) return next();
+  if (!req.body.project_id) {
+    return res.status(400).json({
+      message: "missing required project id"
+    });
+  }
+  if (!project.getById(req.body.project_id)) {
+    return res.status(400).json({
+      message: "please enter a valid project_id"
+    });
+  }
+  if (!req.body.description) {
+    return res.status(400).json({
+      message: "please enter a description"
+    });
+  }
+  if (req.body.description.length > 128) {
+    return res.status(400).json({
+      message: "only 128 characters please"
+    });
+  }
+  if (!req.body.notes) {
+    return res.status(400).json({
+      message: "please enter a couple of notes"
+    });
+  }
+  if (req.body) return next();
 }
 
 function validateProject(req, res, next) {
-    if (!req.body) {
-        return res.status(400).json({
-            message: "missing post data"
-        });
-    }
-    if (!req.body.name) {
-        return res.status(400).json({
-            message: "missing required name field"
-        });
-    }
-    if (!req.body.description) {
-        return res.status(400).json({
-            message: "missing required description field"
-        });
-    }
-    if (req.body) {
-        return next();
-    }
+  if (!req.body) {
+    return res.status(400).json({
+      message: "missing post data"
+    });
+  }
+  if (!req.body.name) {
+    return res.status(400).json({
+      message: "missing required name field"
+    });
+  }
+  if (!req.body.description) {
+    return res.status(400).json({
+      message: "missing required description field"
+    });
+  }
+  if (req.body) {
+    return next();
+  }
 }
--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -2,63 +2,61 @@ const express = require("express");
 const action = require("../data/helpers/actionModel");
 const project = require("../data/helpers/projectModel");
 const db = require("../data/dbConfig");
-const {
-    validateActionId,
-    validateAction
-} = require("../middle/middle.js");
+const { validateActionId, validateAction } = require("../middle/middle.js");
 const router = express.Router();
 router.post("/", validateAction, (req, res) => {
-    action
-        .insert(req.body)
-        .then(newUser => {
-            res.status(201).json(newUser);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                message: 'Could not create action'
-            });
-        });
+  console.log(req.body);
+  action
+    .insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Could not create action"
+      });
+    });
 });
 router.get("/", (req, res) => {
-    action
-        .get()
-        .then(act => {
-            res.status(201).json(act);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                message: "Could not get action"
-            });
-        });
+  action
+    .get()
+    .then(act => {
+      res.status(201).json(act);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Could not get action"
+      });
+    });
 });
 router.get("/:id", validateActionId, (req, res) => {
-    res.status(200).json(req.action)
+  res.status(200).json(req.action);
 });
 router.delete("/:id", validateActionId, (req, res) => {
-    action
-        .remove(req.params.id)
-        .then(user => {
-            res.status(201).json(user);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                message: 'Cannot delete action'
-            });
-        });
+  action
+    .remove(req.params.id)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Cannot delete action"
+      });
+    });
 });
 router.put("/:id", validateAction, (req, res) => {
-    action
-        .update(req.params.id, req.body)
-        .then(user => {
-            res.status(201).json(user);
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'Cannot edit action'
-            });
-        });
+  action
+    .update(req.params.id, req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Cannot edit action"
+      });
+    });
 });
 module.exports = router;
